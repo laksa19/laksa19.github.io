5a0352c8c359f4d43da351ac94cfad9ed620d899
Laksamadi Guko <elru.lakusa@gmail.com>
2020-04-26 05:04:06 +0700

    Update jroute.js

--- assets/js/jroute.js
+++ assets/js/jroute.js
@@ -9,7 +9,8 @@ var iurl = window.location.href;
 var header_id = "md_header";
 var body_id = "md_body";
 var footer_id = "md_footer";
-  		
+
+	
 function lpage(){
 	if(iurl.indexOf("?") > -1){
 	  	let a_ = iurl.split("?")[1].replace(/\//gi,"-");
@@ -20,8 +21,8 @@ function lpage(){
 
 
 function jroute(id,ipage = lpage()){
-
-	$.getJSON("assets/json/page.json?"+timeStamp(), function(result) {
+	let t = '?t=' + new Date().getTime();
+	$.getJSON("assets/json/page.json"+t, function(result) {
 		var page = result;
 		if(!page[ipage] && !ipage){
 			loadDotMD(id,page["home"]);
@@ -36,43 +37,23 @@ function jroute(id,ipage = lpage()){
 
 
 function loadDotMD(id,mdfile){
+	let t = '?t=' + new Date().getTime();
 	if(mdfile.split(/[. ]+/).pop() !== "md"){
-		mdfile = mdfile+".md?"
+		mdfile = mdfile+".md";
 	}else if(mdfile.split(/[. ]+/).pop() == "md"){
-		mdfile = mdfile+"?"
+		mdfile = mdfile;
 	}
-	$.get(mdfile+timeStamp(), function(result) {
+	$.get(mdfile+t, function(result) {
 		let md_result = marked(result);
 		$("#"+id).html(md_result);
 		$("#container").fadeIn(200);
 		$("pre code").each(function(i, e) {hljs.highlightBlock(e)});
 	})
+	
 }
 
 
 
-function timeStamp() {
-	let ts = "";
-	let today = new Date();
-	let hours = today.getHours();
-	let minutes = today.getMinutes();
-	let seconds = today.getSeconds();
-
-	let dd = String(today.getDate()).padStart(2, "0");
-	let mm = Number(today.getMonth())+1;
-	let yyyy = today.getFullYear();
-
-	if (minutes < 10) {
-	    minutes = "0" + minutes;
-	}
-	if (seconds < 10) {
-	    seconds = "0" + seconds;
-	}
-
-	    ts += mm+"-"+dd+"-"+yyyy+"_"+hours + ":" + minutes + ":" + seconds;
-
-	return ts;
-}
 jroute(header_id,"header");
 jroute(body_id);
 jroute(footer_id,"footer");
